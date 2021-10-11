eba038d9f6909cc88e73a84892d465f65871f3fd
Miguel <45602999+mespases@users.noreply.github.com>
2021-10-11 17:48:44 +0200

    listo

--- password.php
+++ password.php
@@ -31,6 +31,12 @@
         border-radius: 10px;
         border: 1px solid #000;
     }
+
+    .mesage {
+        margin-top: 60px;
+        padding: 12px;
+        border: 4px solid #00000026;
+    }
 </style>
 <body>
     <form class="center" method="post" action="password.php">
@@ -41,42 +47,51 @@
         <input class="submit" type="submit"/>
     </form>
 <?php
-    // puede comprobar 1000 passwords por segundo
-    // Cambiar color de fondo y mostrar un mensaje dependiendo de lo segura que sea la contraseña
-    // cada caracter introducido, comprobarlo en ASCII 256 elevado al numero de caracteres
 
-    // pillar contraseña y pasarla a caracteres
-    // comprobar que numero en la tabla del ascii es cada caracter y sumarlos
-
-    // < 1.000 -> instant
-    // 1.000 - 59.999 -> seg
-    // 60.000 - 3.599.999 -> min
-    // 3.599.999 - 84.399.999 -> horas
-    // 34.000.000 - 2.678.399.999 -> dias
-    // 2.678.399.999 ->  meses
-    // -> años
-
-    function howTime($pass) {
-        // modificar todo
-        $longitud = pow(256, strlen($pass));
-
-        if ($longitud < 59999) {
-            changeBackground('red');
-        } else if ($longitud < 3599999) {
-            changeBackground('yellow');
-        }
+    function diccionario() {
+        return $diccionarioPasswords = ["123456", "123456789", "111111", "password", "qwerty", "abc123", "12345678", "password1", "1234567", "123123", "1234567890", "000000",
+            "12345", "iloveyou", "1q2w3e4r5t", "1234", "123456a", "qwertyuiop", "monkey", "123321", "a123456789", "love12", "batman", "internet", "pokemon", "212121",
+            "qweasdzxc", "letmein", "secrert", "xxxxxx", "00000", "valentina", "a1b2c3", "741852963", "austin", "monica", "qaz123", "love1", "loveu", "music1", "jessie",
+            "246810", "midnight", "manchester", "9876543210", "december", "booboo1", "shorty1", "12345abc", "sweety", "hardcore", "cowboys", "sydney", "alex", "scorpio",
+            "1234512345", "qq12345", "qq123456", "onelove", "bond007"];
     }
 
-    function parseTime($pass) {
+    function respuesta($pass) {
         $longitud = pow(256, strlen($pass));
         $timeSeconds = $longitud/1000;
+        $dicc = diccionario();
 
-        if ($timeSeconds < 60) {
+        if ($timeSeconds < 60 || in_array($pass, $dicc)) {
+            setBackground("#E74C3C");
+            echo "<h2 class='center mesage'>Instant</h2>";
+        } else if ($timeSeconds < 3600) {
+            $min = intval($timeSeconds/60);
+            setBackground("#DC7633");
+            echo "<h2 class='center mesage'>".$min." min</h2>";
+        } else if ($timeSeconds < 216000) {
+            $horas = intval(($timeSeconds/60)/60);
+            setBackground("#F4D03F");
+            echo "<h2 class='center mesage'>".$horas." horas</h2>";
+        } else if ($timeSeconds < 5184000) {
+            $dias = intval((($timeSeconds/60)/60)/24);
+            setBackground("#F5B041");
+            echo "<h2 class='center mesage'>".$dias." dias</h2>";
+        } else if ($timeSeconds < 160704000) {
+            $meses = intval(((($timeSeconds/60)/60)/24)/31);
+            setBackground("#2980B9");
+            echo "<h2 class='center mesage'>".$meses." meses</h2>";
+        } else {
+            $years = intval((((($timeSeconds/60)/60)/24)/31)/12);
+            setBackground("#27AE60");
+            echo "<h2 class='center mesage'>".$years." años</h2>";
+        }
 
+        if (in_array($pass, $dicc)) {
+            echo "<p class='center'>Esta contraseña se encuentra dentro de las 100 contraseñas mas usadas</p>";
         }
     }
 
-    function changeBackground($color) {
+    function setBackground($color) {
         echo "<style type='text/css'>
             body {
             background-color: ".$color.";
@@ -87,7 +102,7 @@
     if (isset($_POST["pass"])) {
         $pass = strval($_POST["pass"]);
 
-        howTime($pass);
+        respuesta($pass);
     }
 
 ?>
